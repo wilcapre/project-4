dfcf3395b53de714073c40678bfd7e26b54dd726
willie capre <wcap18@gmail.com>
2019-06-17 18:50:24 -0400

    Create Game.js

---
+++ js/Game.js
@@ -0,0 +1,19 @@
+/* Treehouse FSJS Techdegree
+ * Project 4 - OOP Game App
+ * Game.js */
+class Game{ 
+    missed = 0;
+    phrases = [];
+    activePhrase = null; 
+    constructor (){
+
+    }
+    // constructor (missed, phrases, activePhrase){
+    //     this.missed = missed;
+    //     var player = 0;
+    //     this.phrases = phrases;
+        
+    //     this.activePhrase = activePhrase;
+        
+    // }
+}
